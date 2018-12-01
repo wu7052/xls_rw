3eb37d9aef8a219eaa35c4d4e14ee9787d87469e
wu7052 <17067239@qq.com>
2018-12-01 23:33:57 +0800

    wxpy bot added

--- venv/Scripts/demo1.py
+++ venv/Scripts/demo1.py
@@ -1,7 +1,10 @@
 from xls_rw_test import xls_rw
-
+from wxpy import *
 
 #print ("__name__ %s" % __name__)
 if (__name__ == "__main__"):
     print ("start to xls_rw")
-    xls_rw()
+    # xls_rw()
+    bot = Bot()
+    my_friend = bot.friends().search('Melissa')[0]
+    my_friend.send('睡着了？')
