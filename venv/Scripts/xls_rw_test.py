e0be9e817908b874f469cae36301c18388e26453
wu7052 <17067239@qq.com>
2018-12-03 23:06:34 +0800

    xls process

--- venv/Scripts/xls_rw_test.py
+++ venv/Scripts/xls_rw_test.py
@@ -4,65 +4,97 @@ from xlutils.copy import copy
 import os
 import re
 
+
 def test_file(filename=None):
-    if filename is None :
-        print ("Filename is needed .")
+    if filename is None:
+        print("Filename is needed .")
         return False
     if not os.path.isfile(filename):
-        print ("%s is NOT exist" % filename)
+        print("%s is NOT exist" % filename)
         return False
     if not os.access(filename, os.R_OK):
-        print ("%s is NOT accessible to read" % filename)
+        print("%s is NOT accessible to read" % filename)
         return False
     if not os.access(filename, os.W_OK):
-        print ("%s is NOT accessible to write" % filename)
+        print("%s is NOT accessible to write" % filename)
         return False
     return True
+
 
 def qty_split(main_file=None, src_file=None):
     if not test_file(main_file):
         return -1
     if not test_file(src_file):
         return -1
-    sheet_list = ('文化视窗','互动课堂','录播','物联','安全校园')
+    sheet_list = ['文化视窗', '互动课堂', '录播', '物联', '安全校园']
 
     src_book = xlrd.open_workbook(src_file)
     main_book = xlrd.open_workbook(main_file)
 
-    main_book_wt = copy(main_book) 			# 复制页面
-    # main_book_sheet = main_book_wt.get_sheet(0) 	# 取第一个sheet
-    print(main_book.sheet_names())
+    main_book_wt = copy(main_book)  # 复制页面
+    #print(src_book.sheet_names())
+    #print(main_book.sheet_names())
+
+    src_sheet_list = src_book.sheet_names()
+    main_sheet_list = main_book.sheet_names()
+
+    sheet_map = {}
+    i = 0
+    while len(sheet_list) > 0:
+        sheet_name = r'\w*' + sheet_list[0] + r'\w*'
+        for _ in main_sheet_list:
+            if re.match(sheet_name, _):
+                main_sheet_name = _
+                main_sheet_list.remove(_)
+
+        for _ in src_sheet_list:
+            if re.match(sheet_name, _):
+                src_sheet_name = _
+                src_sheet_list.remove(_)
+
+        sheet_map[i] = [main_sheet_name, src_sheet_name]
+
+        sheet_list.pop(0)
+        i += 1
+
+    print(sheet_map)
+
+    """
     for _ in main_book.sheet_names():
         i =0
         while (i<5):
             sheet_name = r'\w*'+sheet_list[i]+r'\w*'
             if re.match(sheet_name, _):
-                print ("%s is found in sheet_list[%d]"%(_, i))
+                print ("[%s] is found in sheet_list[%d]"%(_, i))
+                # print ("[%s] start to be processed... " % _)
+                # sheet_process(_, main_book_wt, src_book)
             # print ("%d, %s " % (i,  sheet_name))
             i+=1
-        """
-        if (re.match('\w*'+sheet_list[0]+'\w*',_)):
-            print ("sheet_list[0] %s <----> %s"% sheet_list[0], _)
-        elif (re.match(sheet_list[1],_)):
-            print ("sheet_list[1] %s <----> %s"% sheet_list[1], _)
-        elif (re.match(sheet_list[2], _)):
-            print ("sheet_list[2] %s <----> %s"% sheet_list[2], _)
-        elif (re.match(sheet_list[3],_)):
-            print ("sheet_list[3] %s <----> %s"% sheet_list[3], _)
-        elif (re.match(sheet_list[4], _)):
-            print("sheet_list[4] %s <----> %s" % sheet_list[4], _)
-        else:
-            print ("%s dose not match any str" % _)
-        """
-#main_book_sheet =
+    """
+
+
+def sheet_process(sheet_map=None, main_book_handle=None, src_book_handle=None):
+    if sheet_map is None:
+        print("[sheet_proces] need a sheet map \n")
+        return -1
+
+    if main_book_handle is None:
+        print("[sheet_proces] need a main sheet handle \n")
+        return -1
+
+    if src_book_handle is None:
+        print("[sheet_proces] need a source sheet handle \n")
+        return -1
+
+
+
+# main_book_sheet =
 """
     for i in range(96):
         ws.write(1, 5 + i, vector[i])
     # ----- 按(row, col, str)写入需要写的内容 -------
     main_book_wt.save(main_file) 		# 保存文件
 """
-
-
 
 
 def xls_rw():
@@ -76,7 +108,7 @@ def xls_rw():
     # print(sheetlist)
 
     book_info = {}
-    for _ in (sheetlist):
+    for _ in sheetlist:
         sheet = book1.sheet_by_name(_)
         print("Add %s Row %d --- Column %d into Book_info" % (_, sheet.nrows, sheet.ncols))
         book_info[_] = [sheet.nrows, sheet.ncols]
@@ -89,17 +121,17 @@ def xls_rw():
     cur_row = 0
     row = [0, 0, 0, 0]
     rw_row = 0
-    while (cur_row < sheet0.nrows):
+    while cur_row < sheet0.nrows:
         cur_col = 0
         new = 0
-        while (cur_col < sheet0.ncols and cur_col < 4):
-            if (row[cur_col] != sheet0.cell_value(cur_row, cur_col)):
+        while cur_col < sheet0.ncols and cur_col < 4:
+            if row[cur_col] != sheet0.cell_value(cur_row, cur_col):
                 row[cur_col] = sheet0.cell_value(cur_row, cur_col)
                 new = 1
             cur_col += 1
         if (new == 1):
             i = 0
-            for _ in (row):
+            for _ in row:
                 sheet2.write(rw_row, i, _)
                 i += 1
             rw_row += 1
