6b5fbce43a51af8dc9d889e4ab11f1afe97ffda3
wu7052 <17067239@qq.com>
2018-12-04 15:37:42 +0800

    write the title done

--- venv/Scripts/xls_rw_test.py
+++ venv/Scripts/xls_rw_test.py
@@ -29,17 +29,18 @@ def qty_split(main_file=None, src_file=None):
     sheet_list = ['文化视窗', '互动课堂', '录播', '物联', '安全校园']
 
     src_book = xlrd.open_workbook(src_file)
-    main_book = xlrd.open_workbook(main_file)
+    main_book = xlrd.open_workbook(main_file, formatting_info=True)
 
     main_book_wt = copy(main_book)  # 复制页面
-    #print(src_book.sheet_names())
-    #print(main_book.sheet_names())
+    # print(src_book.sheet_names())
+    # print(main_book.sheet_names())
 
     src_sheet_list = src_book.sheet_names()
     main_sheet_list = main_book.sheet_names()
 
     sheet_map = {}
     i = 0
+
     while len(sheet_list) > 0:
         sheet_name = r'\w*' + sheet_list[0] + r'\w*'
         for _ in main_sheet_list:
@@ -58,37 +59,38 @@ def qty_split(main_file=None, src_file=None):
         i += 1
 
     print(sheet_map)
-
-    """
-    for _ in main_book.sheet_names():
-        i =0
-        while (i<5):
-            sheet_name = r'\w*'+sheet_list[i]+r'\w*'
-            if re.match(sheet_name, _):
-                print ("[%s] is found in sheet_list[%d]"%(_, i))
-                # print ("[%s] start to be processed... " % _)
-                # sheet_process(_, main_book_wt, src_book)
-            # print ("%d, %s " % (i,  sheet_name))
-            i+=1
-    """
+    sheet_process(sheet_map, main_book,
+                  main_book_wt, src_book, main_file)
 
 
-def sheet_process(sheet_map=None, main_book_handle=None, src_book_handle=None):
+def sheet_process(sheet_map=None, main_book_rd=None,
+                  main_book_wt=None, src_book_rd=None, main_file=None):
     if sheet_map is None:
         print("[sheet_proces] need a sheet map \n")
         return -1
 
-    if main_book_handle is None:
+    if main_book_rd is None:
         print("[sheet_proces] need a main sheet handle \n")
         return -1
 
-    if src_book_handle is None:
+    if main_book_wt is None:
+        print("[sheet_proces] need a main sheet handle \n")
+        return -1
+
+    if src_book_rd is None:
         print("[sheet_proces] need a source sheet handle \n")
         return -1
 
+    # m_sheet = main_book_handle.sheet_by_name(sheet_map[0][0])
+    m_sheet_wt = main_book_wt.get_sheet(sheet_map[0][0])
+    m_sheet_rd = main_book_rd.sheet_by_name(sheet_map[0][0])
+    print("sheet name:{name}--- ncolumn:{col} --- nrow:{row}".
+          format(name=src_book_rd.sheet_names()[0], row=m_sheet_rd.nrows, col=m_sheet_rd.ncols))
+    m_sheet_wt.write(0, m_sheet_rd.ncols, src_book_rd.sheet_names()[0])
+
+    main_book_wt.save(main_file)
 
 
-# main_book_sheet =
 """
     for i in range(96):
         ws.write(1, 5 + i, vector[i])
@@ -129,7 +131,7 @@ def xls_rw():
                 row[cur_col] = sheet0.cell_value(cur_row, cur_col)
                 new = 1
             cur_col += 1
-        if (new == 1):
+        if new == 1:
             i = 0
             for _ in row:
                 sheet2.write(rw_row, i, _)
