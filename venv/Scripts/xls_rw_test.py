ae216d5fdbc66952a933107d3a710b17cc3af352
wu7052 <17067239@qq.com>
2018-12-05 17:15:00 +0800

    create a map from source file

--- venv/Scripts/xls_rw_test.py
+++ venv/Scripts/xls_rw_test.py
@@ -1,3 +1,5 @@
+from typing import Dict, List, Any, Union
+
 import xlrd
 import xlwt
 from xlutils.copy import copy
@@ -38,7 +40,7 @@ def qty_split(main_file=None, src_file=None):
     src_sheet_list = src_book.sheet_names()
     main_sheet_list = main_book.sheet_names()
 
-    sheet_map = {}
+    sheet_map: Dict[ Union[int, Any], List[Any] ] = {}
     i = 0
 
     while len(sheet_list) > 0:
@@ -53,6 +55,14 @@ def qty_split(main_file=None, src_file=None):
                 src_sheet_name = _
                 src_sheet_list.remove(_)
 
+        """
+        sheet_map 字典格式
+        {0:['目标文件 sheet名称','学校文件 sheet名称']; 
+         1:['目标文件 sheet名称','学校文件 sheet名称'];
+         2:['目标文件 sheet名称','学校文件 sheet名称'];
+         3:['目标文件 sheet名称','学校文件 sheet名称'];
+         4:['目标文件 sheet名称','学校文件 sheet名称'];}
+        """
         sheet_map[i] = [main_sheet_name, src_sheet_name]
 
         sheet_list.pop(0)
@@ -65,6 +75,7 @@ def qty_split(main_file=None, src_file=None):
 
 def sheet_process(sheet_map=None, main_book_rd=None,
                   main_book_wt=None, src_book_rd=None, main_file=None):
+
     if sheet_map is None:
         print("[sheet_proces] need a sheet map \n")
         return -1
@@ -81,16 +92,75 @@ def sheet_process(sheet_map=None, main_book_rd=None,
         print("[sheet_proces] need a source sheet handle \n")
         return -1
 
-    # m_sheet = main_book_handle.sheet_by_name(sheet_map[0][0])
+    src_map = {}
+
+    # 目标表单 写入操作 句柄
     m_sheet_wt = main_book_wt.get_sheet(sheet_map[0][0])
+
+    # 目标表单 读取操作 句柄
     m_sheet_rd = main_book_rd.sheet_by_name(sheet_map[0][0])
-    print("sheet name:{name}--- ncolumn:{col} --- nrow:{row}".
-          format(name=src_book_rd.sheet_names()[0], row=m_sheet_rd.nrows, col=m_sheet_rd.ncols))
+    # print("sheet name:{name}--- ncolumn:{col} --- nrow:{row}".
+    #     format(name=src_book_rd.sheet_names()[0], row=m_sheet_rd.nrows, col=m_sheet_rd.ncols))
+
+    # 在目标表单 写入学校名，并记录 列号
     m_sheet_wt.write(0, m_sheet_rd.ncols, src_book_rd.sheet_names()[0])
+    sheet_map[0].append(m_sheet_rd.ncols)
+    print (sheet_map)
+
+    # 源表单 读取操作句柄
+    src_sheet_rd = src_book_rd.sheet_by_name(sheet_map[0][1])
+    src_map = src_map_gather(src_sheet_rd)
 
     main_book_wt.save(main_file)
 
 
+def src_map_gather(src_sheet_rd=None):
+    if src_sheet_rd is None:
+        print ("[src_map_gather] source sheet read is None , return]")
+        raise excepiton
+    cur_row , content_flag= 0 ,0
+    target_column_index = {'序号':0, '设备':0, '数量':0}
+    target_map={}
+    target_map_index = 0
+    while cur_row < src_sheet_rd.nrows:
+        row_content = src_sheet_rd.row_values(cur_row)
+        index = 0
+
+        # 确定目标列的序号
+        if content_flag == 0:
+            while index < len(row_content):
+                if re.match((r'.*序号.*'),row_content[index]):
+                    target_column_index['序号'] = index
+                    content_flag = 1
+                elif re.match((r'.*设备.*'), row_content[index]):
+                    target_column_index['设备'] = index
+                    content_flag = 1
+                elif re.match((r'.*名称.*'), row_content[index]):
+                    target_column_index['设备'] = index
+                    content_flag = 1
+                elif re.match((r'.*数量.*'), row_content[index]):
+                    target_column_index['数量'] = index
+                    content_flag = 1
+                index += 1
+            """ 测试 列表的index 方法，返回index
+            try:
+                target_column_index['序号'] = row_content.index(r'序号')
+            except Exception as e:
+                pass
+            else:
+                print("found !")
+                content_flag =1
+            """
+        # 已确定目标列，开始逐行读取目标列 写入 map 字典
+        else :
+            #print ("[Target Column] {}".format(target_column_index))
+            target_map[target_map_index]=[row_content[target_column_index['序号']],row_content[target_column_index['设备']],
+                                                                  row_content[target_column_index['数量']]]
+            target_map_index += 1
+        cur_row += 1
+
+    # print (target_map)
+    return target_map
 """
     for i in range(96):
         ws.write(1, 5 + i, vector[i])
